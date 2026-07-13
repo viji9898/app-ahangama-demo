b3e48c6a81998a6c0b8b5754e0de28af1f3f26d5
Oysterr Talent <oysterrtalent@Oysterrs-MacBook-Air.local>
2026-07-13 12:33:11 +0530

    Add upcomingEventsShared data

---
+++ src/data/upcomingEventsShared.js
@@ -0,0 +1,102 @@
+export const SHARED_UPCOMING_EVENT_DAYS = [
+  {
+    key: "2026-07-10",
+    weekday: "Friday",
+    dayNumber: "10",
+    month: "July",
+    events: [
+      {
+        title: "Mono Friday",
+        venue: "Mono Ahangama",
+        time: "8:00 PM - 11:30 PM",
+        category: "Weekly Picks",
+        instagramUrl:
+          "https://www.instagram.com/find_mono_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        image:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783404499/WhatsApp_Image_2026-07-07_at_08.11.41_aygcos.jpg",
+        description: "Played by Sadboi Sam (LK)\nIndie Dance\nGroovy House",
+      },
+      {
+        title: "Weekend Flow DJ Night",
+        venue: "Crust Ahangama",
+        time: "7:00 PM till Dawn",
+        category: "Weekly Picks",
+        instagramUrl:
+          "https://www.instagram.com/crust_ahangama?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        image:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783661172/WhatsApp_Image_2026-07-09_at_18.45.56_k0bwst.jpg",
+        mobileImage:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783665694/Screenshot_2026-07-10_at_12.11.08_jbjrzj.png",
+        description: "Played by DJ Shaggy.\n Pizza, Music & Drinks.",
+      },
+    ],
+  },
+  {
+    key: "2026-07-11",
+    weekday: "Saturday",
+    dayNumber: "11",
+    month: "July",
+    events: [
+      {
+        title: "Pressure Drop",
+        venue: "Mono Ahangama",
+        time: "8:00 PM Onwards",
+        category: "Weekly Picks",
+        instagramUrl:
+          "https://www.instagram.com/find_mono_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        image:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783575568/WhatsApp_Image_2026-07-08_at_20.17.43_avupyv.jpg",
+        description:
+          "Garage, Jungle, DNB & Deep 140.\nFeaturing Janaka Selekta (Sony Music India). \n38 Deep (AUS) & Black Ja (SL).",
+      },
+      {
+        title: "Live Music",
+        venue: "Kai Ahangama",
+        time: "5:00 PM Onwards",
+        category: "Weekly Picks",
+        instagramUrl:
+          "https://www.instagram.com/kai_ahangama?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        image:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783429209/WhatsApp_Image_2026-07-07_at_13.19.27_1_g5wurw.jpg",
+        description: "Live Music with Shenal",
+      },
+    ],
+  },
+  {
+    key: "2026-07-12",
+    weekday: "Sunday",
+    dayNumber: "12",
+    month: "July",
+    events: [
+      {
+        title: "Live Music",
+        venue: "Kai Ahangama",
+        time: "5:00 PM Onwards",
+        category: "Weekly Picks",
+        instagramUrl:
+          "https://www.instagram.com/kai_ahangama?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        image:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783429202/WhatsApp_Image_2026-07-07_at_13.19.27_clnh7a.jpg",
+        description: "Live Music with Jeewaka.",
+      },
+    ],
+  },
+  {
+    key: "2026-07-15",
+    weekday: "Wednesday",
+    dayNumber: "15",
+    month: "July",
+    events: [
+      {
+        title: "A Community Gathering & Working Session",
+        venue: "Flowground, Ahangama",
+        time: "6:00 PM",
+        category: "Weekly Picks",
+        image:
+          "https://res.cloudinary.com/xmybcqyi/image/upload/v1783664769/Screenshot_2026-07-10_at_11.55.19_mfupwl.png",
+        description:
+          "Film screening 'Monsters Cave', guest speakers Dilsiri Welikala (Kite Surfing Lanka), Pearl Protectors & more, facilitated by Marla Lise (The Eco Chapter).",
+      },
+    ],
+  },
+];
