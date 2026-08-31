2ef534eb2e9391ecc49499e9046a1cf46ccabc0a
EniduM <maluddeniyaeniduoushana@gmail.com>
2026-08-31 09:20:09 +0530

    fix: switch map tiles from CARTO (requires API key) to free OpenStreetMap

--- src/pages/ExperienceAhangamaGuide.jsx
+++ src/pages/ExperienceAhangamaGuide.jsx
@@ -611,8 +611,8 @@ function LocatedSection() {
                 style={{ width: "100%", height: "100%", borderRadius: "14px" }}
               >
                 <TileLayer
-                  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
                 <MapResizer />
                 <GuideMapFitBounds places={visiblePlaces} />
